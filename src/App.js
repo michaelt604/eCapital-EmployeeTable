d56aac6b8ceab9764c4ff558d78b4eaaa2898ca1
Michael Ting <michaelt604@gmail.com>
2023-05-22 16:50:30 -0400

    Table Component and temp import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import EmployeeTable from "./EmployeeTable";
 
 function App() {
     return (
         <div className="App">
-            <h1>Employees</h1>
+            <EmployeeTable />
         </div>
     );
 }
