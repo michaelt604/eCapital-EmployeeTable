30bd1084d17cff42eba5807eba3d5e567ec04e7f
Michael Ting <michaelt604@gmail.com>
2023-05-23 17:36:18 -0400

    Log actual error message/status. Standardized queries

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+    connectionLimit: 5,
     host: "72.137.54.82",
     user: "mysql_user",
     password: "mysql_password",
@@ -17,62 +18,94 @@ const db = mysql.createConnection({
 app.post("/importJson", (req, res) => {
     const json = req.body.json;
 
-    const query = `INSERT INTO employees (firstName, lastName, salary) VALUES ?`;
-
-    db.query(query, [json], (error, result) => {
+    db.query(`INSERT INTO employees (firstName, lastName, salary) VALUES ?`, [json], (error, result) => {
         if (error) {
             console.error("Error inserting employees:", error);
-            res.status(500).json({ error: "Failed to insert employees" });
-        } else {
-            console.log("Employees inserted successfully");
-            res.status(200).json({
-                message: "Employees inserted successfully",
-            });
+            return res.status(500).json({ error: "Failed to insert employees" });
         }
+        console.log("Employees inserted successfully");
+        res.status(200).json({
+            message: "Employees inserted successfully",
+        });
     });
 });
 
-//Cleares all employees in the database
+// Clears all employees in the database
 app.post("/cleanDB", (req, res) => {
-    db.query("DELETE FROM employees", (error, result) => {
-        error ? console.log(error) : res.send("Success");
+    db.query(`DELETE FROM employees`, (error, result) => {
+        if (error) {
+            console.error("Error deleting employees:", error);
+            return res.status(500).json({ error: "Failed to delete employees" });
+        }
+
+        console.log("Employees deleted successfully");
+        res.status(200).json({
+            message: "Employees deleted successfully",
+        });
     });
 });
 
 //Add new employee to the database
 app.post("/addEmployee", (req, res) => {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const salary = req.body.salary;
+    const { firstName, lastName, salary } = req.body;
 
     db.query(`INSERT INTO employees (firstName, lastName, salary) VALUES (?, ?, ?)`, [firstName, lastName, salary], (error, result) => {
-        error ? console.log(error) : res.send("Success");
+        if (error) {
+            console.error("Error adding employee:", error);
+            return res.status(500).json({ error: "Failed to add employee" });
+        }
+
+        console.log("Employee added successfully");
+        res.status(200).json({
+            message: "Employee added successfully",
+        });
     });
 });
 
 //Edits a selected employee in the database using its id
 app.post("/editEmployee", (req, res) => {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const salary = req.body.salary;
-    const id = req.body.id;
+    const { firstName, lastName, salary, id } = req.body;
+
     db.query(`UPDATE employees SET firstName = ?, lastName = ?, salary = ? WHERE id = ?`, [firstName, lastName, salary, id], (error, result) => {
-        error ? console.log(error) : res.send("Success");
+        if (error) {
+            console.error("Error editing employee:", error);
+            return res.status(500).json({ error: "Failed to edit employee" });
+        }
+
+        console.log("Employee edited successfully");
+        res.status(200).json({
+            message: "Employee edited successfully",
+        });
     });
 });
 
 //Delete a selected employee from the database using its id
 app.post("/deleteEmployee", (req, res) => {
     const id = req.body.id;
+
     db.query(`DELETE FROM employees WHERE id =?`, [id], (error, result) => {
-        error ? console.log(error) : res.send("Success");
+        if (error) {
+            console.error("Error deleting employee:", error);
+            return res.status(500).json({ error: "Failed to delete employee" });
+        }
+
+        console.log("Employee deleted successfully");
+        res.status(200).json({
+            message: "Employee deleted successfully",
+        });
     });
 });
 
 //Request to get all employees from the database
 app.get("/getEmployee", (req, res) => {
-    db.query("SELECT * FROM employees", (error, result) => {
-        error ? console.log(error) : res.send(result);
+    db.query(`SELECT * FROM employees`, (error, result) => {
+        if (error) {
+            console.error("Error fetching employees:", error);
+            return res.status(500).json({ error: "Failed to fetch employees" });
+        }
+
+        console.log("Employees fetched successfully");
+        res.status(200).json(result);
     });
 });
 
